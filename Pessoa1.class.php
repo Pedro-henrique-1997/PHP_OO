f8a86af03250bd1947a61c9f5b3ab72d550dc826
Pedro Henrique <pedrofuturama620@gmail.com.com>
2021-12-15 20:35:03 -0300

    second commit

---
+++ Pessoa1.class.php
@@ -0,0 +1,28 @@
+<?php
+
+class Pessoa{
+	public $Codigo;
+	public $Nome;
+	public $Altura;
+	public $Idade;
+	public $Nascimento;
+	public $Escolaridade;
+	public $Salario;
+
+	function Crescer($centimetros){
+		if($centimetros > 0){
+			$this->Altura += $centimetros;
+		}
+	}
+
+	function Formar($titulacao){
+		$this->Escolaridade = $titulacao;
+	}
+
+	function Envelhecer($anos){
+		if($anos > 0)
+			$this->Idade += $anos;
+	}
+}
+
+?>
